3706924d9c57cb40a5bacbf4d08190e509f16b92
Rick Hills <student@epicodus.com>
2015-09-22 11:23:25 -0700

    DB connection

--- app/app.php
+++ app/app.php
@@ -16,9 +16,17 @@
     $app['debug'] = true;
 
 
-    $server = 'mysql:host=localhost;dbname=lifecoach';
-    $username = 'root';
-    $password = '';
+    // $server = 'mysql:host=localhost;dbname=lifecoach';
+    // $username = 'root';
+    // $password = '';
+
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"], 1);
+    $server = 'mysql:host='.$host.';dbname='.$db;
     $DB = new PDO($server, $username, $password);
 
 
